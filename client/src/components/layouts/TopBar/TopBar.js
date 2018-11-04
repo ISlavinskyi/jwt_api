329db5b7e1183e8980ecd38ab520f4d4cfcb5edb
Ivan <islavinskyi@gmail.com>
2018-11-04 23:25:37 +0200

    client side structure/choose logo

---
+++ client/src/components/layouts/TopBar/TopBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './TopBar.css'
+
+const TopBar = () => {
+    return (
+        <div className='TopBar'>
+            <div className='wrapper'>
+                <div className='Logo'>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TopBar;
--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
+
+import TopBar from '../components/layouts/TopBar/TopBar';
+
 import './App.css';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
+                <TopBar/>
                 Hello from app
             </div>
         );
